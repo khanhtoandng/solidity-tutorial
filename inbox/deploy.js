80ca692ddd23e3e24c5e03f7d46eebf53dba19a5
Toan Huynh <toan.huynh@team.enouvo.com>
2022-01-20 02:06:41 +0700

    lottery: basic smart contract

--- inbox/deploy.js
+++ inbox/deploy.js
@@ -3,7 +3,10 @@ const Web3 = require("web3");
 
 const { abi, evm } = require("./compile");
 
-provider = new HDWalletProvider("YOUR_MNEMONIC", "YOUR_INFURA_URL");
+provider = new HDWalletProvider(
+  "0xAB51Dc2d5571022623AC54db02E7780e9F5a8Cfb",
+  "https://rinkeby.infura.io/v3/d68e27fed1074986b85ddffc9f1074b1"
+);
 
 const web3 = new Web3(provider);
 
